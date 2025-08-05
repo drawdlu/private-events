d3f9a3eab5d5797b4143fb7f3d77f59a61b5aead
drawdlu <152858099+drawdlu@users.noreply.github.com>
2025-08-05 20:12:03 +0800

    feat: Create form for new events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,23 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = current_user.events.build(event_params)
+
+    if @event.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def event_params
+    params.require(:event).permit(:location, :date)
+  end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,6 @@
 class Event < ApplicationRecord
+  validates :location, presence: true
+  validates :date, presence: true
+
   belongs_to :creator, class_name: "User", foreign_key: "user_id"
 end
