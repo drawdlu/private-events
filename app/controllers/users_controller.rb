dd808c314fa38f70db9eeea7af8103aeb67df99c
drawdlu <152858099+drawdlu@users.noreply.github.com>
2025-08-05 18:47:14 +0800

    feat: Only allow user to view on show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,18 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :view_own_page_only, only: [ :show ]
   def show
     @events = User.find(params[:id]).events
+  end
+
+  private
+
+  def view_own_page_only
+    @user = User.find(params[:id])
+
+    if current_user != @user
+      flash[:notice] = "You can only view your own user page"
+      redirect_to root_path
+    end
   end
 end
