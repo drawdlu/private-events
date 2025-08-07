c2422c659d13c3c606be0bbc3c44c6866e2da4e6
drawdlu <152858099+drawdlu@users.noreply.github.com>
2025-08-07 19:34:23 +0800

    feat: Separate past and upcoming events on index

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,4 +5,13 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: "User", foreign_key: "user_id"
   has_many :guests
   has_many :attendees, through: :guests, source: :user
+
+
+  def self.upcoming
+    where("date >= ?", Date.current)
+  end
+
+  def self.past
+    where("date < ?", Date.current)
+  end
 end
