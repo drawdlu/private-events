5c8b213661d411d809c4f95fa48261398e6774e8
drawdlu <152858099+drawdlu@users.noreply.github.com>
2025-08-09 22:29:27 +0800

    feat: Invite other users

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -1,17 +1,41 @@
 class GuestsController < ApplicationController
+  def new
+    @event = Event.find(guest_params["event_id"])
+    @users = User.all
+    @guest = Guest.new
+  end
   def create
-    @guest = current_user.guests.build(guest_params)
+    @access = guest_params["access"]
+
+    if @access == "attending"
+      @guest = current_user.guests.build(guest_params)
+    else
+
+      @user = User.find(guest_params["user_id"])
+      @guest = @user.guests.build(guest_params)
+    end
 
     if @guest.save
-      flash[:notice] = "You have successfully signed up for the event"
-      redirect_to root_path
+      if @access == "attending"
+        flash[:notice] = "You have successfully signed up for the event"
+        redirect_to root_path
+      else
+        @event = Event.find(@guest.event_id)
+        @users = User.all
+        redirect_to new_guest_path
+      end
     else
       render "events/show", locals: { event: @event }, status: :unprocessable_entity
     end
   end
 
   def destroy
-    @guest = current_user.guests.find_by(guest_params)
+    if guest_params["access"] == "attending"
+      @guest = current_user.guests.find_by(guest_params)
+    else
+      @guest = Guest.find_by(guest_params)
+    end
+
     @guest.destroy
 
     flash[:notice] = "Successfully unattended event"
@@ -21,6 +45,6 @@ class GuestsController < ApplicationController
   private
 
   def guest_params
-    params.permit(:event_id, :access)
+    params.permit(:event_id, :access, :user_id)
   end
 end
