3eb980e55aeaefc8eaf822f1b9d3b73ea664cff0
drawdlu <152858099+drawdlu@users.noreply.github.com>
2025-08-09 23:12:09 +0800

    refactor: Remove unnecessary logic from controller
    Handled by views

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -2,30 +2,22 @@ class GuestsController < ApplicationController
   def new
     @event = Event.find(guest_params["event_id"])
     @users = User.all
-    @guest = Guest.new
   end
   def create
-    @access = guest_params["access"]
-
-    if @access == "attending"
+    if guest_params["access"] == "attending"
       @guest = current_user.guests.build(guest_params)
-    else
 
+      @guest.save
+
+      flash[:notice] = "You have successfully signed up for the event"
+      redirect_to event_path(guest_params["event_id"])
+    else
       @user = User.find(guest_params["user_id"])
       @guest = @user.guests.build(guest_params)
-    end
 
-    if @guest.save
-      if @access == "attending"
-        flash[:notice] = "You have successfully signed up for the event"
-        redirect_to root_path
-      else
-        @event = Event.find(@guest.event_id)
-        @users = User.all
-        redirect_to new_guest_path
-      end
-    else
-      render "events/show", locals: { event: @event }, status: :unprocessable_entity
+      @guest.save
+
+      redirect_to new_guest_path
     end
   end
 
@@ -35,7 +27,7 @@ class GuestsController < ApplicationController
       @guest.destroy
 
       flash[:notice] = "Successfully unattended event"
-      redirect_to root_path status: :see_other
+      redirect_to event_path(guest_params["event_id"])
     else
       @guest = Guest.find_by(guest_params)
       @guest.destroy
