51216df1f61d75e4d75e7b27eed516386375875b
drawdlu <152858099+drawdlu@users.noreply.github.com>
2025-08-11 18:54:45 +0800

    feat: Add event invitations not yet attended

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,16 @@ class User < ApplicationRecord
   has_many :guests, dependent: :destroy
   has_many :attended_events, -> { where("guests.access = ?", 0) },
              through: :guests, source: :event
+
+  has_many :invited_events, -> { where("guests.access = ?", 1) },
+              through: :guests, source: :event
+
+  def invited_events_not_attended
+    @events_invited = self.invited_events.upcoming
+    @events_attending = self.attended_events.upcoming
+
+    @invited_events_not_attended = @events_invited - @events_attending
+
+    Event.where(id: @invited_events_not_attended.map(&:id))
+  end
 end
